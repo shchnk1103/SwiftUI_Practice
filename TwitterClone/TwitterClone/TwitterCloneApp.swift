0f13ccacc13749ba7a477161642fb286fa3f2e51
shchnk1103 <761962425@qq.com>
2022-12-14 20:47:40 +0800

    init Twitter app clone

---
+++ TwitterClone/TwitterClone/TwitterCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwitterCloneApp.swift
+//  TwitterClone
+//
+//  Created by DoubleShy0N on 2022/12/14.
+//
+
+import SwiftUI
+
+@main
+struct TwitterCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
