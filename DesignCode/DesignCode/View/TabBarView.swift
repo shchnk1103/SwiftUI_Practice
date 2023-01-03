3611f3062e00122eb92fb1189db54f13ecb23208
DoubleShy0N <doubleshy0n@DoubleShy0Ns-MacBook-Pro.local>
2023-01-03 20:50:55 +0800

    add TabBarView

---
+++ DesignCode/DesignCode/View/TabBarView.swift
@@ -0,0 +1,52 @@
+//
+//  TabBarView.swift
+//  DesignCode
+//
+//  Created by DoubleShy0N on 2023/1/3.
+//
+
+import SwiftUI
+
+struct TabBarView: View {
+    var body: some View {
+        ZStack(alignment: .bottom) {
+            ContentView()
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+                .offset(y: 300)
+            
+            HStack {
+                Spacer()
+                VStack(spacing: 0) {
+                    Image(systemName: "house")
+                        .symbolVariant(.fill)
+                        .font(.body.bold())
+                        .frame(width: 80, height: 29)
+                    Text("Learn Now")
+                        .font(.caption2)
+                }
+                Spacer()
+                VStack(spacing: 0) {
+                    Image(systemName: "magnifyingglass")
+                        .symbolVariant(.fill)
+                        .font(.body.bold())
+                        .frame(width: 80, height: 29)
+                    Text("Explore")
+                        .font(.caption2)
+                }
+                Spacer()
+            }
+            .padding(.top, 14)
+            .frame(height: 88, alignment: .top)
+            .background(.ultraThinMaterial, in: RoundedRectangle(cornerRadius: 34, style: .continuous))
+            .strokeStyle(cornerRadius: 34)
+            .frame(maxHeight: .infinity, alignment: .bottom)
+            .ignoresSafeArea()
+        }
+    }
+}
+
+struct TabBarView_Previews: PreviewProvider {
+    static var previews: some View {
+        TabBarView()
+    }
+}
