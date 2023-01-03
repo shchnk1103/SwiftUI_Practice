240fdd6d626e92b4a4a44b28b8726ed7c0b9f031
DoubleShy0N <doubleshy0n@DoubleShy0Ns-MacBook-Pro.local>
2023-01-03 20:12:21 +0800

    add animation

--- DesignCode/DesignCode/AccountView.swift
+++ DesignCode/DesignCode/AccountView.swift
@@ -42,7 +42,12 @@ struct AccountView: View {
                 .background(
                     HexagonView()
                         .offset(x: -50, y: -100)
-            )
+                )
+                .background(
+                    BlobView()
+                        .offset(x: 200, y: 0)
+                        .scaleEffect(0.6)
+                )
             
             Text("DoubleShy0N")
                 .font(.title.weight(.semibold))
