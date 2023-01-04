4dcd72ebef74678cee38dbfe078476bbe64a3cf4
DoubleShy0N <doubleshy0n@DoubleShy0Ns-MacBook-Pro.local>
2023-01-04 16:21:36 +0800

    geometryreader and prefrencekey

--- DesignCode/DesignCode/Model/TabModel.swift
+++ DesignCode/DesignCode/Model/TabModel.swift
@@ -12,13 +12,14 @@ struct TabItem: Identifiable {
     var text: String
     var icon: String
     var tab: Tab
+    var color: Color
 }
 
 var tabItems = [
-    TabItem(text: "Learn", icon: "house", tab: .home),
-    TabItem(text: "Explore", icon: "magnifyingglass", tab: .explore),
-    TabItem(text: "Notifications", icon: "bell", tab: .notification),
-    TabItem(text: "Library", icon: "rectangle.stack", tab: .library)
+    TabItem(text: "Learn", icon: "house", tab: .home, color: .teal),
+    TabItem(text: "Explore", icon: "magnifyingglass", tab: .explore, color: .blue),
+    TabItem(text: "Notifications", icon: "bell", tab: .notification, color: .red),
+    TabItem(text: "Library", icon: "rectangle.stack", tab: .library, color: .pink)
 ]
 
 enum Tab: String {
@@ -26,4 +27,11 @@ enum Tab: String {
     case explore
     case notification
     case library
+}
+
+struct TabPreferenceKey: PreferenceKey {
+    static var defaultValue: CGFloat = 0
+    static func reduce(value: inout CGFloat, nextValue: () -> CGFloat) {
+        value = nextValue()
+    }
 }
--- DesignCode/DesignCode/View/TabBarView.swift
+++ DesignCode/DesignCode/View/TabBarView.swift
@@ -9,6 +9,8 @@ import SwiftUI
 
 struct TabBarView: View {
     @State private var selectedTab: Tab = .home
+    @State private var color: Color = .teal
+    @State private var tabItemWidth: CGFloat = 0
     
     var body: some View {
         ZStack(alignment: .bottom) {
@@ -27,33 +29,101 @@ struct TabBarView: View {
             .frame(maxWidth: .infinity, maxHeight: .infinity)
             
             HStack {
-                ForEach(tabItems) { item in
-                    Button {
-                        selectedTab = item.tab
-                    } label: {
-                        VStack(spacing: 0) {
-                            Image(systemName: item.icon)
-                                .symbolVariant(.fill)
-                                .font(.body.bold())
-                                .frame(width: 44, height: 29)
-                            
-                            Text(item.text)
-                                .font(.caption2)
-                                .lineLimit(1)
-                        }
-                        .frame(maxWidth: .infinity)
-                    }
-                    .foregroundStyle(selectedTab == item.tab ? .primary : .secondary)
-                }
+                buttons
             }
             .padding(.horizontal, 8)
             .padding(.top, 14)
             .frame(height: 88, alignment: .top)
-            .background(.ultraThinMaterial, in: RoundedRectangle(cornerRadius: 34, style: .continuous))
+            .background(
+                .ultraThinMaterial,
+                in: RoundedRectangle(cornerRadius: 34, style: .continuous)
+            )
+            .background(
+                background
+            )
+            .overlay(
+                overlay
+            )
             .strokeStyle(cornerRadius: 34)
             .frame(maxHeight: .infinity, alignment: .bottom)
             .ignoresSafeArea()
         }
+    }
+    
+    var buttons: some View {
+        ForEach(tabItems) { item in
+            Button {
+                withAnimation(.spring(response: 0.3, dampingFraction: 0.7)) {
+                    color = item.color
+                    selectedTab = item.tab
+                }
+            } label: {
+                VStack(spacing: 0) {
+                    Image(systemName: item.icon)
+                        .symbolVariant(.fill)
+                        .font(.body.bold())
+                        .frame(width: 44, height: 29)
+                    
+                    Text(item.text)
+                        .font(.caption2)
+                        .lineLimit(1)
+                }
+                .frame(maxWidth: .infinity)
+            }
+            .foregroundStyle(selectedTab == item.tab ? .primary : .secondary)
+            .blendMode(selectedTab == item.tab ? .overlay : .normal)
+            .overlay {
+                GeometryReader { geometry in
+                    Color.clear.preference(key: TabPreferenceKey.self, value: geometry.size.width)
+                }
+            }
+            .onPreferenceChange(TabPreferenceKey.self) { value in
+                tabItemWidth = value
+            }
+        }
+    }
+    
+    var background: some View {
+        HStack {
+            if selectedTab == .explore { Spacer() }
+            if selectedTab == .notification {
+                Spacer()
+                Spacer()
+            }
+            if selectedTab == .library { Spacer() }
+            Circle().fill(color).frame(width: tabItemWidth)
+            if selectedTab == .home { Spacer() }
+            if selectedTab == .explore {
+                Spacer()
+                Spacer()
+            }
+            if selectedTab == .notification { Spacer() }
+        }
+        .padding(.horizontal, 8)
+    }
+    
+    var overlay: some View {
+        HStack {
+            if selectedTab == .explore { Spacer() }
+            if selectedTab == .notification {
+                Spacer()
+                Spacer()
+            }
+            if selectedTab == .library { Spacer() }
+            Rectangle()
+                .fill(color)
+                .frame(width: 28, height: 5)
+                .cornerRadius(3)
+                .frame(width: tabItemWidth)
+                .frame(maxHeight: .infinity, alignment: .top)
+            if selectedTab == .home { Spacer() }
+            if selectedTab == .explore {
+                Spacer()
+                Spacer()
+            }
+            if selectedTab == .notification { Spacer() }
+        }
+        .padding(.horizontal, 8)
     }
 }
 
