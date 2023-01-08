968e0a9ca204856dafbc11460c3db22ee091ea20
shchnk <761962425@qq.com>
2023-01-08 16:38:16 +0800

    add custom progress view

---
+++ DesignCode/DesignCode/Components/PlayView.swift
@@ -0,0 +1,20 @@
+//
+//  PlayView.swift
+//  DesignCode
+//
+//  Created by DoubleShy0N on 2023/1/8.
+//
+
+import SwiftUI
+
+struct PlayView: View {
+    var body: some View {
+        Image(systemName: "play.fill")
+    }
+}
+
+struct PlayView_Previews: PreviewProvider {
+    static var previews: some View {
+        PlayView()
+    }
+}
